f40c907ce9a0c389f6ee5044459fa7cd91731508
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-05-31 16:30:21 +0530

    Updated main.py and app.py file

--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,35 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request
+from openai import OpenAI
+import os
 
 app = FastAPI()
 
+client = OpenAI(
+    api_key=os.getenv("API_KEY"),
+    base_url=os.getenv("BASE_URL")
+)
 
 @app.get("/")
 def home():
     return {"message": "Backend Running Successfully"}
 
 @app.post("/questions")
-async def Questions(req : Request):
+async def questions(req: Request):
+
     data = await req.json()
+
     prompt = data["prompt"]
 
-    return{
-        "message" : "Promt received",
-        "prompt" : prompt
-    }
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
 
+    return {
+        "response": response.choices[0].message.content
+    }
--- Frontend/app.py
+++ Frontend/app.py
@@ -1,45 +1,45 @@
 import streamlit as st
-from openai import OpenAI
+import requests
 
 server_url = st.secrets["backend_url"]
 
-client = OpenAI(
-    api_key=st.secrets["api_key"],
-    base_url=st.secrets["base_url"]
-)
 st.title("AI Interview Chat Bot")
+
 with st.form("Details"):
+
     Lang = st.text_input("Enter the Language")
     Topic = st.text_input("Enter the Topic")
-    Level = st.selectbox("Enter the Level",["Easy","Medium","Advance"])
-    Type = st.selectbox("Enter the Type",["MCQ's","Theory Questions","Coding Snippets"])
+
+    Level = st.selectbox(
+        "Enter the Level",
+        ["Easy", "Medium", "Advanced"]
+    )
+
+    Type = st.selectbox(
+        "Enter the Type",
+        ["MCQ's", "Theory Questions", "Coding Snippets"]
+    )
 
     Submit_Button = st.form_submit_button("Submit")
 
     if Submit_Button:
 
         prompt = f"""
-            Give me Questions in {Lang} Language on Topic Name is {Topic} in {Level} level and {Type} type.
-            
-            Follow:
-            Do not give any expalination give only Questions
+Give me Questions in {Lang} Language on Topic Name is {Topic}
+in {Level} level and {Type} type.
 
-        """
-      
+Follow:
+Do not give any explanation.
+Give only questions.
+"""
 
-        response = client.chat.completions.create(
-            model = "llama-3.1-8b-instant",
-            messages = [
-                {
-                    "role" : "user",
-                    "content" : prompt
-                    
-                } 
-            ]
+        response = requests.post(
+            server_url,
+            json={
+                "prompt": prompt
+            }
         )
-        st.write(response.choices[0].message.content)
-        
 
+        data = response.json()
 
-
-
+        st.write(data["response"])
